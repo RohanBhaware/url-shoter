46f1755bcae65151d6b8e85612c830896bc69b81
RohanBhaware <bhawarerohan24@gmail.com>
2025-09-17 22:59:12 +0530

    github fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/url-shoter",
+  base: "/url-shoter/",
   resolve: {
     alias: {
       // react: require.resolve('react'),
